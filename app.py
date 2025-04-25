5cd20bc9625ddec9a7a297ef8393a438d92da4b4
CPettifo <craig_petti4@hotmail.com>
2025-04-25 09:09:43 +0200

    remove app run

--- app.py
+++ app.py
@@ -46,7 +46,3 @@ def is_valid_api_key(key):
     #cursor.close()
     #conn.close()
     #return result is not None
-
-if __name__== "__main__":
-    port = int(os.environ.get("PORT", 8000))  
-    app.run(host="0.0.0.0", port=port)
